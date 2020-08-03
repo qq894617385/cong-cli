339b5dcd1423ed4c6dc90ccac7284ac3847b2ddf
yejiancong <894617385@qq.com>
2020-08-03 17:29:20 +0800

    init

---
+++ main.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+
+const cmd = require("commander")
+const chalk = require('chalk')
+// const downGit = require('./src/downLoad')
+// const options = require('./src/options')
+
+cmd.command('init').description('初始化模板').action(async (args) => {
+    // typeof args !== 'string' && (console.log(chalk.red('缺少必填参数')), process.exit(1))
+    console.log(chalk.yellow('初始化 \n'))
+    // 填选项
+    // await options()
+    // // 拉取
+    // await downGit(args)
+})
+
+cmd.parse(process.argv)
